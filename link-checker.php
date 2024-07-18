f275630af6aff9027f4cbadeb44964c426480c53
bram <github@brambring.nl>
2024-07-18 11:08:10 +0200

    changed namespace

--- link-checker.php
+++ link-checker.php
@@ -36,8 +36,8 @@ GNU General Public License for more details.
 You should have received a copy of the GNU General Public License
 along with Broken Link Checker. If not, see https://www.gnu.org/licenses/gpl-2.0.html.
 */
+namespace Blc\Component\Blc\Administrator\Blc;
 
-namespace WPMUDEV_BLC;
 
 // If this file is called directly, abort.
 defined( 'WPINC' ) || die;
